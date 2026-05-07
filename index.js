1284414255ca5cd2683ac2d8a61816bf5309f146
saoooood <aldhbhanysyd11@gmail.com>
2026-05-08 02:21:57 +0300

    🛡️ Saeed Bot v3 - Initial Release

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function startSaeedBot() {
     // طلب كود الربط إذا لم يكن الحساب مسجلاً
     if (!sock.authState.creds.registered) {
         // --- ضع رقمك هنا بالصيغة الدولية بدون + ---
-        const myNumber = "96777xxxxxxx"; 
+        const myNumber = "967770179625"; 
 
         console.log(`\n⏳ جاري طلب كود الربط للرقم: ${myNumber}...`);
         
